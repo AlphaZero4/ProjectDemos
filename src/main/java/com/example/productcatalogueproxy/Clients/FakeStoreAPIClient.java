cf9a23cc49b8e69a58db8079e23b6155924df272
yobro004 <rohithmanda24@gmail.com>
2024-04-27 14:32:52 +0530

    functional-1

---
+++ src/main/java/com/example/productcatalogueproxy/Clients/FakeStoreAPIClient.java
@@ -0,0 +1,2 @@
+package com.example.productcatalogueproxy.Clients;public class FakeStoreAPIClient {
+}
---
+++ src/main/java/com/example/productcatalogueproxy/Controller/ControllerAdvise.java
@@ -0,0 +1,2 @@
+package com.example.productcatalogueproxy.Controller;public class ControllerAdvise {
+}
